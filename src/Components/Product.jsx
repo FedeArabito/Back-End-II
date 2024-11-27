839cdc19c43b43a3b7360165c0a21564d9e3be69
Federico Arabito <federicoarabito1999@gmail.com>
2024-11-26 21:45:52 -0300

    starting to add styles to products componentes

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Product = ({product}) => {
   return (
-    <div>
+    <div className='flex flex-row justify-center items-center border-2 border-black'>
        <div>
-        <img src={product.image} alt="" />
+        <img src={product.image} alt="" className='max-w-[200px]'/>
         <h1>{product.title}</h1>
-        <p>{product.price}</p>
+        <p>${product.price}</p>
        </div>
         
     </div>
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -3,8 +3,10 @@ import Product from './Product.jsx'
 
 const ProductList = ({data}) => {
   return (
-    <div>
+    <div className='flex justify-center items-center' >
+    <div className='grid grid-cols-4 gap-4 mt-10 '>
       {data.map((product) => <Product key={product.id} product={product} />)}
+    </div>
     </div>
   )
 }
